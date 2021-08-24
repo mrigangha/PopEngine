d0070fa0fc593410ac5bf791d5e7ca2280781036
LogicX <67688715+mrigangha@users.noreply.github.com>
2021-08-24 16:18:25 +0530

    Delete 3dGame/src/com/3dGame directory

--- 3dGame/src/com/3dGame/src/com/PopEngine/IGameLogic.java
+++
@@ -1,8 +0,0 @@
-package com.PopEngine;
-
-public interface IGameLogic {
-    public void InitGame();
-    public void UpdateGameState(double dt);
-    public void render(Window window);
-    public void CleanUp();
-}
